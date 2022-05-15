b15550ffd4abcbf11f3e8ed06453d6b6740b4e2a
muhammadsayedmorsi <muhammadsayed1001@yahoo.com>
2022-05-16 00:05:40 +0200

    add pages

---
+++ main.js
@@ -0,0 +1,13 @@
+// Playing vidoe and pause it..
+// const video = document.querySelector(".hero-video")
+// const btn = document.querySelector(".play-pause")
+
+// function togglePlayPause() {
+//   if (video.paused) {
+//     btn.classList.add("play")
+//     video.play()
+//   } else if (video.play) {
+//     btn.classList.remove("play")
+//     video.pause()
+//   }
+// }
